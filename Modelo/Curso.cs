afa9438565a6708b53877644c47dedc7b0846c09
Bruno Santos Queiroz <brunosantista95@live.com>
2019-05-01 19:32:17 -0300

    Teste

---
+++ Modelo/Curso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Curso
+    {
+        public long? CursoId { get; set; }
+        public string CursoNome { get; set; }
+        public string CursoDescricao { get; set; }
+    }
+}
---
+++ Modelo/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Usuarios
+    {
+        public long? UsuarioId { get; set; }
+        public string UsuarioNome { get; set; }
+        public string UsuarioSobrenome { get; set; }
+        public string UsuarioSenha { get; set; }
+        public string UsuarioEmail { get; set; }
+        public string UsuarioCPF { get; set; }
+        public string UsuarioTelefone { get; set; }
+    }
+}

75dfd453fd319b868dc54d28d4831218f843eac7
Bruno Santos Queiroz <brunosantista95@live.com>
2019-05-02 11:48:37 -0300

    02/05/2019 11:48

---
+++ Persistencia/Contexts/EFContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Persistencia.Contexts
+{
+    class EFContext:DbContext
+    {
+        public EFContext():base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+                new DropCreateDatabaseIfModelChanges<EFContext>() //Faz com que o BD seja recriado toda vez que uma modificação acontecer
+                );
+        }
+
+        public DbSet<Cargo> Cargos { get; set; }
+    }
+}

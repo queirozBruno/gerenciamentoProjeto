c7357ab8205c4b142cf47ee7eb4977fd9f4fa07c
Bruno Santos Queiroz <brunosantista95@live.com>
2019-05-07 20:06:19 -0300

    07/05/2019 20: 06

--- Persistencia/DAL/CargoDAL.cs
+++ Persistencia/DAL/CargoDAL.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Persistencia.Contexts;
+using Modelo;
 
 namespace Persistencia.DAL
 {
     class CargoDAL
     {
+        private EFContext context = new EFContext();
+        
+        public void CriarCargo(Cargo cargo)
+        {
+
+        }
     }
 }
